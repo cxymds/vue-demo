d06976333489ba4b9cf6c5d2d961d7f2e0782793
cxymds <cxymds@qq.com>
2020-06-19 11:40:52 +0800

    vue 杂项

---
+++ vue-5/demo/src/views/Demo.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="father" @click="cls1">
+		<div class="child" @click.stop="cls2"></div>
+		<button ref="btn">按钮</button>
+		
+		<img src="" alt="" ref="image">
+	</div>
+</template>
+
+<script>
+export default{
+	name:"Demo",
+	data:function(){
+		return {
+			
+		}
+	},
+	methods:{
+		cls1(){
+			console.log(process.env);
+		},
+		cls2(){
+			console.log(this.$refs.btn.innerText='123')
+			
+		}
+		
+	}
+}
+</script>
+
+<style scoped>
+	.father{
+		width: 200px;
+		height: 200px;
+		background-color: red;
+	}
+	.child{
+		width: 100px;
+		height: 100px;
+		background-color: #7FFFD4;
+	}
+</style>
